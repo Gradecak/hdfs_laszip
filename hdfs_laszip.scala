cba14c1bfc61329e21c55399e178cc361b2e2446
Marijan Gradecak <gradecam@tcd.ie>
2018-10-02 21:11:12 +0200

    test

--- hdfs_laszip.scala
+++ hdfs_laszip.scala
@@ -13,11 +13,20 @@ class LasZip(bin_path : String){
 
 
   def decompress(filepath : String, outfile_path : String){
+    val t0 = System.nanoTime()
     hdfs.copyToLocalFile(false,
                          new Path(filepath),
                          new Path(outfile_path))
     val res = Seq(bin_path, outfile_path, "-o", "decompresed.las").!!
+    if(res != ""){
       println(res)
+    }
+    //remove file from local after copying to HDFS
+    hdfs.copyFromLocalFile(true,
+                       new Path("./decompressed.las"),
+                           new Path(outfile_path))
+    val t1 = System.nanoTime()
+    println("decompress time: " + (t1 - t0)/1000000 + "ms")
     //TODO decompress file, return decomressed file path
   }
 
