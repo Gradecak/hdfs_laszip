c1c59882d878b974e89505d367490b341cf74df6
Marijan Gradecak <gradecam@tcd.ie>
2018-10-05 10:24:21 +0200

    a

--- hdfs_laszip.scala
+++ hdfs_laszip.scala
@@ -5,47 +5,66 @@ import org.apache.hadoop.fs.{FileAlreadyExistsException, FileSystem, FileUtil, P
 import org.apache.hadoop.conf.Configuration
 import java.nio.file.Files
 import sys.process._
+import java.io.File
 
-class LasZip(bin_path : String){
+class LasTools(bin_base_path : String){
 
   //use to read and write from/to hadoop
   val hdfs = FileSystem.get(new Configuration())
+  val homeDir = sys.env("HOME")
+  val tmpDir = homeDir + "/tmp"
 
+  //lasTools binaries
+  val laszip = bin_base_path + "/laszip"
+  val lasmerge = bin_base_path + "/lasmerge"
+
+
+  def mergeTile(tileFolder: String){
+    decompressFolder(tileFolder)
+    merge(tmpDir + "/*.las", Some(tileFolder.split('/').last + ".las"));
+  }
 
   def decompressFolder(folderpath:String){
-    // val dir = Files.createTempDirectory("laz")
+
+    new File(tmpDir).mkdirs(); //create temp directory
     hdfs.copyToLocalFile(false,
                          new Path(folderpath),
-                         new Path("./tmp_cp/"))
-    this.decompress("./tmp_cp/*.laz", "$HOME/mega.las")
-  }
-
-
-  private[this] def decompress(in:String, out:String){
-    Seq(bin_path, "-i", in, "-o", out).!!
-  }
-
-  private[this] def decompressM(in:Seq[String], out:String){
-    val cmd = (bin_path +: in) ++ Seq("-o", out)
-    cmd.!!
-  }
-
-  def decompressFile(filepath : String, outfile_path : String){
-    hdfs.copyToLocalFile(false,
-                         new Path(filepath),
-                         new Path(outfile_path))
-    val res = Seq(bin_path, outfile_path, "-o", "decompressed.las").!!
-    if(res != ""){
-      println(res)
-    }
-    //remove file from local after copying to HDFS
+                         new Path(tmpDir))
+    this.decompress(tmpDir, None)
     hdfs.copyFromLocalFile(true,
-                           new Path("./decompressed.las"),
-                           new Path(outfile_path))
-    // println("decompress time: " + (t1 - t0)/1000000 + "ms")
-    outfile_path
+                           new Path(tmpDir),
+                           new Path(folderpath))
   }
 
+  private[this] def merge(in:String, out:Option[String]){
+    out match{
+      case Some(o) => Seq(lasmerge, "-i", in, "-o", o).!!
+      case None    => Seq(lasmerge, "-i", in).!!
+    }
+  }
+
+  private[this] def decompress(in:String, out:Option[String]){
+    out match{
+      case Some(o) => Seq(laszip, "-i", in, "-o", o).!!
+      case None    => Seq(laszip, "-i", in).!!
+    }
+  }
+
+  // def decompressFile(filepath : String, outfile_path : String){
+  //   hdfs.copyToLocalFile(false,
+  //                        new Path(filepath),
+  //                        new Path(outfile_path))
+  //   val res = Seq(bin_path, outfile_path, "-o", "decompressed.las").!!
+  //   if(res != ""){
+  //     println(res)
+  //   }
+  //   //remove file from local after copying to HDFS
+  //   hdfs.copyFromLocalFile(true,
+  //                          new Path("./decompressed.las"),
+  //                          new Path(outfile_path))
+  //   // println("decompress time: " + (t1 - t0)/1000000 + "ms")
+  //   outfile_path
+  // }
 
   def compress(filepath : String, outfile_path : String){
     //TODO
